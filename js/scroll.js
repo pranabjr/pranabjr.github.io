9c109cc8ac3ff8bd1444950404d21bfc9d9b8eae
Pranab Jung Rana <pranabjr@gmail.com>
2016-04-12 00:14:59 +0200

    scroll.js changed

--- js/scroll.js
+++ js/scroll.js
@@ -151,13 +151,23 @@ function adjust_textarea(h) {
 
 
 //for map
-
-function initialize() {
-  var mapProp = {
-    center:new google.maps.LatLng(27.7172,85.3240),
-    zoom:5,
-    mapTypeId:google.maps.MapTypeId.ROADMAP
+var myCenter=new google.maps.LatLng(27.703466,85.3111983);
+function initialize()
+{
+var mapProp = {
+  center:myCenter,
+  zoom:15,
+  mapTypeId:google.maps.MapTypeId.ROADMAP
   };
-  var map=new google.maps.Map(document.getElementById("googleMap"), mapProp);
+
+var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+var marker=new google.maps.Marker({
+  position:myCenter,
+  animation:google.maps.Animation.BOUNCE
+  });
+
+marker.setMap(map);
 }
+
 google.maps.event.addDomListener(window, 'load', initialize);
